a0cce36bd14a2cb96d1d27ffb88573b784daccdc
Rogelio Cobian <rogeliocobian2@gmail.com>
2018-06-13 16:15:49 -0700

    worked on binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
-  monsterName: string = 'Kaiju';
+export class AppComponent implements OnInit {
+ title = 'app';
+ // we use this on monster
+ // monsterName:string = 'kaiju';
+ // monsterPower: number;
+
+hero: any = {
+  name: 'Internet',
+  power: null
+};
+
+ monsters: any[] = [
+   {
+
+     monsterName: 'kaiju',
+     monsterPower: null
+   },
+   {
+
+     monsterName: 'smaug',
+     monsterPower: null
+   }
+ ];
+
+ monsterSelected;
+ ngOnInit() {
+   this.pickMonster();
+
+ }
+ pickMonster() {
+   const index = Math.floor(Math.random() * this.monsters.length);
+   this.monsterSelected = this.monsters[index];
+ }
+
+ generatePower() {
+  this.monsterSelected.monsterPower = Math.floor(Math.random() * 1000);
+
+ }
+ generateHeroPower() {
+  this.monsterSelected.monsterPower = Math.floor(Math.random() * 1000);
+
+ }
 }
